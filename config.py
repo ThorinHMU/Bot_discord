c85cb63bcf4a8269da672b993aa6ea9adbb16f27
ThorinHMU <118696034+ThorinHMU@users.noreply.github.com>
2024-03-03 15:48:44 +0100

    Update config.py
    token

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ class SQL:
 sql_mc = SQL("88.99.61.194", 3306,
           "u8_AEZRh5Wfhn", "KBNVzuOsC+0TIQC!e!2O4fxe",
           "s8_Activity")
-TOKEN = "OTQ1MDI5NzkwNTI2NzYzMDA5.GMnxD9.xO6gLMWSTCFq2RokZWWsr063Kk0I02uruTiBN4"
+TOKEN = ${{ secrets.TOKEN }}
