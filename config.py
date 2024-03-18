a3c3b5fb91412c2b6cf6d157e6efc9b40287d448
ThorinHMU <118696034+ThorinHMU@users.noreply.github.com>
2024-03-18 18:11:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ sql_mc = SQL("88.99.61.194", 3306,
           "u8_AEZRh5Wfhn", "KBNVzuOsC+0TIQC!e!2O4fxe",
           "s8_Activity")
 TOKEN = "OTQ1MDI5NzkwNTI2NzYzMDA5.GMnxD9.xO6gLMWSTCFq2RokZWWsr063Kk0I02uruTiBN4"
+prefixe = "t."
